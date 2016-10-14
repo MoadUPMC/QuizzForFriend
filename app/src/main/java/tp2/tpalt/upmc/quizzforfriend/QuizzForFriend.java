905ceda9ddaecbb8b138047a4341fc04812fcc39
Maxime <maxim@g-lob.com>
2016-10-14 13:36:09 +0200

    Full screen

--- app/src/main/java/tp2/tpalt/upmc/quizzforfriend/QuizzForFriend.java
+++ app/src/main/java/tp2/tpalt/upmc/quizzforfriend/QuizzForFriend.java
@@ -1,13 +1,20 @@
 package tp2.tpalt.upmc.quizzforfriend;
 
 import android.app.Application;
+import android.content.Context;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.net.Uri;
 
+import java.io.File;
 import java.util.List;
 
 /**
  * Created by macbookpro on 10/10/2016.
  */
 public class QuizzForFriend extends Application {
+
+    private MediaPlayer mediaPlayer;
 
     private List<Partie> parties;
 
@@ -20,5 +27,30 @@ public class QuizzForFriend extends Application {
 
     public List<Partie> getParties() {
         return parties;
+    }
+
+    public MediaPlayer getMediaPlayer() {
+        return mediaPlayer;
+    }
+
+    public void audioPlayerFromPath(String path, String fileName){
+        //set up MediaPlayer
+        MediaPlayer mp = new MediaPlayer();
+
+        try {
+            mp.setDataSource(path + File.separator + fileName);
+            mp.prepare();
+            mp.start();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    public void audioPlayer(int path){
+        if(this.mediaPlayer != null && this.mediaPlayer.isPlaying()) return;
+        this.mediaPlayer = MediaPlayer.create(getApplicationContext(), path);
+        this.mediaPlayer.start();
+        this.mediaPlayer.setLooping(true);
     }
 }
