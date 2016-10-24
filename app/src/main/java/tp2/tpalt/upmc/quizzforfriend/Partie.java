aa5079d97b32fe0b85e26fa67adb5523d6d92691
Unknown <souri.moad@gmail.com>
2016-10-24 22:27:10 +0200

    Rien a dire

--- app/src/main/java/tp2/tpalt/upmc/quizzforfriend/Partie.java
+++ app/src/main/java/tp2/tpalt/upmc/quizzforfriend/Partie.java
@@ -10,6 +10,7 @@ public class Partie {
 
     private List<Question> questions;
     private Joker joke;
+    private String theme;
 
     public Partie(List<Question> q){
         this.questions = q;
@@ -23,4 +24,8 @@ public class Partie {
     public Joker getJoke() {
         return joke;
     }
+
+    public String getTheme() {return theme; }
+
+    public void setTheme(String theme) {this.theme = theme; }
 }
